126488e74d437ad13fca2cd7dca4a08273ef01b5
haoheliu <867390095@qq.com>
2022-03-28 02:14:27 +0100

    update

--- resources/energy_normalize.py
+++ resources/energy_normalize.py
@@ -5,6 +5,6 @@ import numpy as np
 from tqdm import tqdm 
 
 for file in tqdm(glob("*/*.wav") + glob("*/*/*.wav") + glob("*/*/*/*.wav")):
-    x,rate = librosa.load(file)
+    x,rate = librosa.load(file, sr=None)
     x = x/np.max(np.abs(x))
     sf.write(file,x,samplerate=rate)
